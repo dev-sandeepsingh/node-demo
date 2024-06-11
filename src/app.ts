4d4969fba64e9d09fe7df2aa5d2492dbf31d3145
dev-sandeepsingh <sandeepsingh@founderandlightning.com>
2024-06-11 11:54:07 +0530

    Change endpoint message

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const host = '127.0.0.1';
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Welcome to express!!');
+    res.send('Hello Dev, Welcome to express!!');
 });
 
 app.get('/health', (req: Request, res: Response) => {
